53fbf3e3fac481a65f0bd90089f2cfc32198d556
fxyan <1350821504@qq.com>
2018-08-23 21:05:07 +0800

    一个定长的Array

---
+++ Array.py
@@ -0,0 +1,30 @@
+class Array(object):
+    def __init__(self, size=32):
+        self._size = size
+        self._item = [None] * size
+
+    # def __len__(self):
+    #     return self._size
+
+    def __getitem__(self, index):
+        return self._item[index]
+
+    def __setitem__(self, key, value):
+        self._item[key] = value
+
+    def clear(self, value=None):
+        for i in range(len(self._item)):
+            self._item[i] = value
+
+    def __iter__(self):
+        for item in self._item:
+            yield item
+
+    def __repr__(self):
+        return '列表值: {}'.format(self._item)
+
+size = 10
+xx = Array(size)
+xx[0] = 2
+print(len(xx))
+
